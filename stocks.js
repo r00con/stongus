33f9ff91d4c1791951bcd5772ba71b4081ea4b1f
r00con <nrroshan02@gmail.com>
2026-01-19 21:37:28 +0800

    Update stocks.js

--- stocks.js
+++ stocks.js
@@ -1,9 +1,7 @@
-const STOCKS = {
-  dingdong: { name: "Ding Dong Co.", price: 30 },
-  applejuice: { name: "Apple juice", price: 5 },
-  mojang: { name: "Mojang Airlines", price: 50 },
-  unfairprice: { name: "Unfairprice", price: 25 },
-  scammobilez: { name: "ScamMobilez", price: 70 }
+module.exports = {
+  "Ding Dong Co.": 30,
+  "Apple juice": 5,
+  "Mojang Airlines": 50,
+  "Unfairprice": 25,
+  "ScamMobilez": 70
 };
-
-module.exports = STOCKS;
