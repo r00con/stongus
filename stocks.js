807ffb0dea3228fbec85b7b65b9cd05459704806
r00con <nrroshan02@gmail.com>
2026-01-19 21:00:14 +0800

    Update stocks.js

--- stocks.js
+++ stocks.js
@@ -1,7 +1,9 @@
-module.exports = {
+const STOCKS = {
   "Ding Dong Co.": 30,
   "Apple juice": 5,
   "Mojang Airlines": 50,
   "Unfairprice": 25,
   "ScamMobilez": 70
 };
+
+module.exports = STOCKS;
