b584af66b9b02e8295322e8e21bea727c59be35f
r00con <nrroshan02@gmail.com>
2026-01-19 21:38:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname));
 
 const STOCKS = require("./stocks");
 
-/* 🔒 ALLOWED ACCOUNTS */
+/* 🔒 ALLOWED USERS */
 const ACCOUNTS = {
   LockedIn: "H73Yyr7",
   ragavan67: "oJD785k",
@@ -30,6 +30,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "landing.html"));
 });
 
+/* LOGIN */
 app.post("/login", (req, res) => {
   const { user, pass } = req.body;
   if (ACCOUNTS[user] !== pass) return res.json({ ok: false });
@@ -37,67 +38,47 @@ app.post("/login", (req, res) => {
   res.json({ ok: true });
 });
 
+/* PLAYER */
 app.get("/player", (req, res) => {
-  res.json(players[req.query.user] || null);
+  const user = req.query.user;
+  if (!players[user]) return res.json(null);
+  res.json(players[user]);
 });
 
-/* SEND STOCKS */
+/* STOCK LIST */
 app.get("/stocks", (req, res) => {
-  const out = {};
-  Object.keys(STOCKS).forEach(id => {
-    out[id] = STOCKS[id].price;
-  });
-  res.json(out);
+  res.json(STOCKS);
 });
 
 /* BUY */
 app.post("/buy", (req, res) => {
-  const { user, stockId } = req.body;
+  const { user, stock } = req.body;
   ensurePlayer(user);
 
-  const stock = STOCKS[stockId];
-  if (!stock) return res.json({ ok: false });
+  const price = STOCKS[stock];
+  if (price === undefined) return res.json({ ok: false });
 
-  if (players[user].money < stock.price)
-    return res.json({ ok: false });
+  if (players[user].money < price) return res.json({ ok: false });
 
-  players[user].money -= stock.price;
-  players[user].stocks[stockId] =
-    (players[user].stocks[stockId] || 0) + 1;
+  players[user].money -= price;
+  players[user].stocks[stock] =
+    (players[user].stocks[stock] || 0) + 1;
 
   res.json({ ok: true });
 });
 
 /* SELL */
 app.post("/sell", (req, res) => {
-  const { user, stockId } = req.body;
-  if (!players[user]?.stocks[stockId]) return res.json({ ok: false });
+  const { user, stock } = req.body;
+  if (!players[user]?.stocks[stock]) return res.json({ ok: false });
 
-  players[user].money += STOCKS[stockId].price;
-  players[user].stocks[stockId]--;
+  players[user].money += STOCKS[stock];
+  players[user].stocks[stock]--;
 
-  if (players[user].stocks[stockId] === 0)
-    delete players[user].stocks[stockId];
+  if (players[user].stocks[stock] === 0)
+    delete players[user].stocks[stock];
 
   res.json({ ok: true });
-});
-
-/* FRIENDS */
-app.get("/friends", (req, res) => {
-  const me = req.query.me;
-  ensurePlayer(me);
-
-  res.json(
-    Object.keys(players)
-      .filter(p => p !== me)
-      .map(p => ({
-        name: p,
-        money: players[p].money,
-        stocks: Object.keys(players[p].stocks).map(
-          id => STOCKS[id].name
-        )
-      }))
-  );
 });
 
 app.listen(process.env.PORT || 3000);
