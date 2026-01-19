e13f53fc3e334bc1662954c8de70a03fb6534ce9
r00con <nrroshan02@gmail.com>
2026-01-19 18:31:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+
 app.use(express.json());
 app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 3000;
 
-// 👇 IMPORT STOCK LOGIC
+// IMPORT STOCK LOGIC
 const { registerStockRoutes, players } = require("./stocks");
 
 // REGISTER STOCK ROUTES
@@ -14,23 +15,25 @@ registerStockRoutes(app);
 /* =========================
    PAGES
    ========================= */
-app.get("/", (_, res) => res.sendFile(__dirname + "/landing.html"));
-app.get("/gamepage.html", (_, res) => res.sendFile(__dirname + "/gamepage.html"));
-app.get("/friends.html", (_, res) => res.sendFile(__dirname + "/friends.html"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/landing.html"));
+app.get("/login", (req, res) => res.sendFile(__dirname + "/login.html"));
+app.get("/gamepage.html", (req, res) => res.sendFile(__dirname + "/gamepage.html"));
+app.get("/friends.html", (req, res) => res.sendFile(__dirname + "/friends.html"));
+app.get("/stockspage.html", (req, res) => res.sendFile(__dirname + "/stockspage.html"));
 
 /* =========================
-   FRIENDS DATA (FIXED)
+   FRIENDS DATA (NAMES ONLY)
    ========================= */
 app.get("/friends-data", (req, res) => {
   const me = req.query.me;
   if (!players[me]) return res.json([]);
 
   const friends = Object.keys(players)
-    .filter(p => p !== me)
-    .map(p => ({
-      name: p,
-      money: players[p].money,
-      stocks: players[p].stocks
+    .filter(name => name !== me)
+    .map(name => ({
+      name,
+      money: players[name].money,
+      stocks: Object.keys(players[name].stocks)
     }));
 
   res.json(friends);
@@ -40,5 +43,5 @@ app.get("/friends-data", (req, res) => {
    START SERVER
    ========================= */
 app.listen(PORT, () => {
-  console.log("Stongus server running on", PORT);
+  console.log("Stongus server running on port", PORT);
 });
